317b024876b010520c68f3ed3413a07c800307dc
luudantas <luana.vsdantas@gmail.com>
2026-05-08 16:28:46 -0300

    .

--- questao3.c
+++ questao3.c
@@ -5,6 +5,11 @@ int main()
     int x;
     int cont=0;
     FILE *arq = fopen("arquivo2.txt","r");
+    if (arq==NULL)
+    {
+        printf("O arquivo nao existe, por favor crie-o antes de executar o programa");
+        return 1;
+    }
     while((x = fgetc(arq))!=EOF)
     {
         if(x == 'a' || x == 'A') cont++;
